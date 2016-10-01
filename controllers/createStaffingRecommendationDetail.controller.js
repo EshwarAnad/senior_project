e124595e2c274e568628202e5ee51e3b0093c342
andysterks <apsterks@gmail.com>
2016-10-01 17:53:29 -0600

    A great deal of progress

--- controllers/createStaffingRecommendationDetail.controller.js
+++ controllers/createStaffingRecommendationDetail.controller.js
@@ -1,15 +1,18 @@
 angular.module('tps').controller('CreateStaffingRecommendationDetailCtrl', createStaffingRecommendationDetailController);
 
-function createStaffingRecommendationDetailController(staffingRequestId, $scope, $location, CandidatesConstants, StaffingRequestsService){
+function createStaffingRecommendationDetailController(staffingRequestId, $scope, $location, StaffingRecommendationsService,
+                                                      CandidatesConstants, StaffingRequestsService, $rootScope, ROUTES){
 
     $scope.title = 'Create Staffing Recommendation Details';
 
     $scope.staffingRequest = StaffingRequestsService.getById(staffingRequestId);
+    console.log($scope.staffingRequest);
 
     $scope.candidates = CandidatesConstants.list;
 
     $scope.goTo = goTo;
     $scope.stopProp = stopProp;
+    $scope.create = create;
 
     function goTo(path){
         $location.path(path);
@@ -22,5 +25,21 @@ function createStaffingRecommendationDetailController(staffingRequestId, $scope,
         } else if(candidate){
             //candidate.selected = !candidate.selected;
         }
+    }
+
+    function create(){
+        var candidatesToAdd = [];
+
+        for(var i = 0; i < $scope.candidates.length; i++){
+            if($scope.candidates[i].selected){
+                candidatesToAdd.push($scope.candidates[i]);
+            }
+        }
+
+        StaffingRecommendationsService.create($scope.staffingRequest.company,
+            $rootScope.session, candidatesToAdd, $scope.staffingRequest,
+            []);
+
+        $location.path(ROUTES.STAFFING_RECOMMENDATIONS_LIST);
     }
 }
