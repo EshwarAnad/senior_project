e0b267b36664278bcbb93032281f55fca9766407
andysterks <apsterks@gmail.com>
2016-10-01 19:26:52 -0600

    More trying stuff.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-angular.module('tps', ['ngRoute'])
+angular.module('tps', ['ngRoute', 'tps.ROUTES'])
 
     .run(function(SessionService){
         var credential = {
@@ -15,6 +15,8 @@ angular.module('tps', ['ngRoute'])
     })
 
     .config(function($routeProvider, ROUTES){
+        console.log(ROUTES);
+
         $routeProvider
             .when(ROUTES.HOME, {
                 controller: 'DashboardCtrl',
@@ -111,4 +113,23 @@ angular.module('tps', ['ngRoute'])
                     }
                 }
             })
+    });
+
+angular.module('tps.ROUTES', [])
+    .constant('ROUTES', {
+        CANDIDATES_DETAIL: '/candidate/:id',
+        CANDIDATES_LIST: '/candidates',
+        HIRING_COMPANIES_DETAIL: '/hiringcompany/:id',
+        HIRING_COMPANIES_LIST: '/hiringcompanies',
+        HOME: '/',
+        LOGIN: '/login',
+        STAFFING_RECOMMENDATIONS_CREATE_DETAIL: '/staffing/recommendations/create/detail/:staffingRequestId',
+        STAFFING_RECOMMENDATIONS_CREATE_LIST: '/staffing/recommendations/create/',
+        STAFFING_RECOMMENDATIONS_DETAIL: '/staffing/recommendation/:id',
+        STAFFING_RECOMMENDATIONS_LIST: '/staffing/recommendations/',
+        STAFFING_REQUESTS_CREATE: '/staffing/requests/create/',
+        STAFFING_REQUESTS_DETAIL: '/staffing/requests/detail/:id',
+        STAFFING_REQUESTS_LIST: '/staffing/requests/',
+        USER_DETAIL: '/user/:id',
+        USER_LIST: '/users'
     });
