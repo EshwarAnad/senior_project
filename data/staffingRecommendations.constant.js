6ccccaac61a219b5667a902691bb8eecfb761364
andysterks <apsterks@gmail.com>
2016-10-15 16:28:47 -0600

    WIP.

--- data/staffingRecommendations.constant.js
+++ data/staffingRecommendations.constant.js
@@ -205,48 +205,40 @@ angular.module('tps').constant('StaffingRecommendationsConstant', {
                 {
                     id: 9001,
                     details: {
-                        id: 1002,
-                        skills: 'Basketball, Dunking',
+                        id: 1003,
+                        skills: 'Copy-writing, Microsoft Word, Microsoft Excel',
                         status: 'EmployedJobSearching',
-                        notes: [],
-                        privateNotes: [],
-                        recommendations: [],
-                        user: {
-                            id: 1003,
-                            skills: 'Copy-writing, Microsoft Word, Microsoft Excel',
-                            status: 'EmployedJobSearching',
-                            notes: [
+                        notes: [
 
-                            ],
-                            privateNotes: [
-                                {
-                                    content: 'This is a very interesting candidate.  I believe he should be hired quickly.',
-                                    author: {
-                                        id: 1001,
-                                        username: 'sallystaffer',
-                                        firstName: 'Sally',
-                                        lastName: 'Staffer',
-                                        type: 'staffing',
-                                        hiringCompany: null,
-                                        emailAddress: 'sally@tpsstaffing.com',
-                                        password: 'password1234'
-                                    },
-                                    created: new Date(2016, 7, 29, 11, 17, 11, 13)
-                                }
-                            ],
-                            recommendations: [
-
-                            ],
-                            user: {
-                                id: 1004,
-                                username: 'christoperCandidate',
-                                firstName: 'Christopher',
-                                lastName: 'Candidate',
-                                type: 'candidate',
-                                hiringCompany: null,
-                                emailAddress: 'ccandidate@gmail.com',
-                                password: 'password1234'
+                        ],
+                        privateNotes: [
+                            {
+                                content: 'This is a very interesting candidate.  I believe he should be hired quickly.',
+                                author: {
+                                    id: 1001,
+                                    username: 'sallystaffer',
+                                    firstName: 'Sally',
+                                    lastName: 'Staffer',
+                                    type: 'staffing',
+                                    hiringCompany: null,
+                                    emailAddress: 'sally@tpsstaffing.com',
+                                    password: 'password1234'
+                                },
+                                created: new Date(2016, 7, 29, 11, 17, 11, 13)
                             }
+                        ],
+                        recommendations: [
+
+                        ],
+                        user: {
+                            id: 1004,
+                            username: 'christoperCandidate',
+                            firstName: 'Christopher',
+                            lastName: 'Candidate',
+                            type: 'candidate',
+                            hiringCompany: null,
+                            emailAddress: 'ccandidate@gmail.com',
+                            password: 'password1234'
                         }
                     }
                 }
