0a04c694dcea460725ad894e4896874bd271f52f
andysterks <apsterks@gmail.com>
2016-10-21 08:31:42 -0600

    WIP.

--- data/users.constant.js
+++ data/users.constant.js
@@ -106,7 +106,19 @@ angular.module('tps').constant('UsersConstants', {
                 ],
                 recommendations: [
 
-                ]
+                ],
+                user: {
+                    id: 1004,
+                    username: 'christoperCandidate',
+                    firstName: 'Christopher',
+                    lastName: 'Candidate',
+                    type: 'candidate',
+                    hiringCompany: null,
+                    emailAddress: 'ccandidate@gmail.com',
+                    password: 'password1234'
+                },
+                income: 33000,
+                location: 'Columbus, Ohio, United States'
             }
         },
         {
@@ -128,7 +140,19 @@ angular.module('tps').constant('UsersConstants', {
                 ],
                 recommendations: [
 
-                ]
+                ],
+                user: {
+                    id: 1005,
+                    username: 'michaeljordan',
+                    firstName: 'Michael',
+                    lastName: 'Jordan',
+                    type: 'candidate',
+                    hiringCompany: null,
+                    emailAddress: 'ccandidate@gmail.com',
+                    password: 'password1234'
+                },
+                income: 230000,
+                location: 'Charlotte, North Carolina, United States'
             }
         },
         {
@@ -150,7 +174,19 @@ angular.module('tps').constant('UsersConstants', {
                 ],
                 recommendations: [
 
-                ]
+                ],
+                user: {
+                    id: 1006,
+                    username: 'elonmusk',
+                    firstName: 'Elon',
+                    lastName: 'Musk',
+                    type: 'candidate',
+                    hiringCompany: null,
+                    emailAddress: 'elonMusk@teslamotors.com',
+                    password: 'password1234'
+                },
+                income: 1000000,
+                location: 'Huntington Beach, California, United States'
             }
         },
         {
@@ -187,7 +223,46 @@ angular.module('tps').constant('UsersConstants', {
                 ],
                 recommendations: [
 
-                ]
+                ],
+                user: {
+                    id: 1007,
+                    username: 'andysterks',
+                    firstName: 'Andy',
+                    lastName: 'Sterkowitz',
+                    type: 'candidate',
+                    hiringCompany: null,
+                    emailAddress: 'andys@gmail.com',
+                    password: 'password1234',
+                    candidateInfo: {
+                        id: 1000,
+                        skills: 'C#, AngularJS, Javascript',
+                        status: 'UnemployedJobSearching',
+                        notes: [
+
+                        ],
+                        privateNotes: [
+                            {
+                                content: 'An awesome candidate!  He should get hired somewhere...',
+                                author: {
+                                    id: 1001,
+                                    username: 'sallystaffer',
+                                    firstName: 'Sally',
+                                    lastName: 'Staffer',
+                                    type: 'staffing',
+                                    hiringCompany: null,
+                                    emailAddress: 'sally@tpsstaffing.com',
+                                    password: 'password1234'
+                                },
+                                created: new Date(2016, 8, 7, 11, 17, 11, 13)
+                            }
+                        ],
+                        recommendations: [
+
+                        ]
+                    }
+                },
+                income: 45000,
+                location: 'Chicago, IL, United States'
             }
         }
     ]
